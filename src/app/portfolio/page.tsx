a743769d3237a7b665714ba454fbd3161565d5ef
ScissorHands26 <ryan.scissons@gmail.com>
2024-04-28 15:23:25 -0400

    Update Portfolio page with image and video additions

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 export default function Portfolio() {
   return (
@@ -14,16 +16,69 @@ export default function Portfolio() {
           Portfolio
         </h1>
       </div>
-      <div className="grid grid-cols-3 gap-1">
-        {Array.from({ length: 9 }).map((_, index) => (
-          <div key={index} className="flex justify-center items-center">
-            <img
-              src="https://via.placeholder.com/400x300"
-              alt="Placeholder"
-              className="max-w-full max-h-full"
+      <div className="grid grid-cols-3 gap-4 mb-8">
+        <div className="flex flex-col items-center p-4 bg-slate-200 rounded-lg">
+          <h3 className="text-3xl">KOLD Intern Anchor Intro | April 8, 2024</h3>
+          <iframe
+            width="500"
+            height="280"
+            src={"https://www.youtube.com/embed/ewz-h8kvdhA"}
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="flex flex-col items-center p-4 bg-slate-200 rounded-lg">
+          <h3 className="text-3xl">UA Theatre Program Shutdown</h3>
+          <iframe
+            width="500"
+            height="280"
+            src={
+              "https://www.youtube.com/embed/-DUAcD0HZm0?si=9L3igtk-4qfglzkZ"
+            }
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="flex flex-col items-center p-4 bg-slate-200 rounded-lg">
+          <h3 className="text-3xl">Tucson Election Night 2023</h3>
+          <iframe
+            width="500"
+            height="280"
+            src={
+              "https://www.youtube.com/embed/Mkr-dX95304?si=YJjtyq1_4s2l50Aj"
+            }
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="flex flex-col items-center p-4 bg-slate-200 rounded-lg">
+          <h3 className="text-3xl">Nightfall at Old Tucson</h3>
+          <iframe
+            width="500"
+            height="280"
+            src={
+              "https://www.youtube.com/embed/l85FuJvHKMo?si=eWVWibP905YaACkr"
+            }
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <Link
+          href="https://azluminaria.org/2024/04/01/forced-to-flee-civil-war-this-syrian-mother-found-a-new-home-in-tucson"
+          target="_blank"
+        >
+          <div className="flex flex-col items-center p-4 bg-slate-200 rounded-lg">
+            <h3 className="text-3xl">
+              Refugee mother finds new home in Tucson
+            </h3>
+            <Image
+              width={500}
+              height={280}
+              src="/story-cover-01.png"
+              alt="People walking"
             />
           </div>
-        ))}
+        </Link>
       </div>
     </div>
   );
