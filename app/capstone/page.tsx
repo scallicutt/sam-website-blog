e15826b0bbac604aa8d151bf61a921cbc4492772
ScissorHands26 <ryan.scissons@gmail.com>
2025-04-17 21:52:00 -0400

    Updated app

--- app/capstone/page.tsx
+++ app/capstone/page.tsx
@@ -56,65 +56,6 @@ export default function CapstonePage() {
           et que simaximil dolore.
         </p>
       </div>
-
-      {/* Footer */}
-      <div className="bg-[#0A1931] text-white py-8">
-        <div className="container mx-auto px-4">
-          <div className="flex justify-between items-center">
-            {/* Social Links */}
-            <div className="flex items-center gap-4">
-              <h3 className="font-fino-sans text-2xl">FOLLOW ME</h3>
-              <div className="flex gap-4">
-                <Link href="https://linkedin.com" target="_blank">
-                  <Image
-                    src="/assets/linkedin.svg"
-                    alt="LinkedIn"
-                    width={24}
-                    height={24}
-                  />
-                </Link>
-                <Link href="https://twitter.com" target="_blank">
-                  <Image
-                    src="/assets/twitter.svg"
-                    alt="Twitter"
-                    width={24}
-                    height={24}
-                  />
-                </Link>
-                <Link href="https://facebook.com" target="_blank">
-                  <Image
-                    src="/assets/facebook.svg"
-                    alt="Facebook"
-                    width={24}
-                    height={24}
-                  />
-                </Link>
-                <Link href="https://instagram.com" target="_blank">
-                  <Image
-                    src="/assets/instagram.svg"
-                    alt="Instagram"
-                    width={24}
-                    height={24}
-                  />
-                </Link>
-                <Link href="https://youtube.com" target="_blank">
-                  <Image
-                    src="/assets/youtube.svg"
-                    alt="YouTube"
-                    width={24}
-                    height={24}
-                  />
-                </Link>
-              </div>
-            </div>
-            {/* Copyright */}
-            <div className="text-sm font-obviously-light">
-              <p>© 2025 SCallicutt. All Rights Reserved</p>
-              <p>Website design and dev by S. Callicutt</p>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
